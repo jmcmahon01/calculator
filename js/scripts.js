1daf593193a9b9f439129b1c69bd0087a56eb287
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-10-26 11:03:33 -0700

    use arithmetic functions and return user input in result

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,31 @@
 function add(number1, number2) {
   return number1 + number2;
 }
+function sub(number1, number2) {
+  return number1 - number2;
+}
+function mult(number1, number2) {
+  return number1 * number2;
+}
+function div(number1, number2) {
+  return number1 / number2;
+}
+
 //User Interface Logic
 const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-window.alert(add(number1, number2));
+const number2 = parseInt(prompt("Enter another number:"))
+
+
+
+
+window.alert(number1 + " + " + number2 + " = " + add(number1, number2) + "\n"
+  + number1 + " - " + number2 + " = " + sub(number1, number2) + "\n");
+
+
+
+
+
+
+
 
 
